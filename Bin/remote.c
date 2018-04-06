e60a9e534611d56ec8ef11c147df475dfb712ff2
varuneagle555 <varunroyeagle@outlook.com>
2018-04-06 18:46:28 -0400

    Biil fixed remote.c

--- Bin/remote.c
+++ Bin/remote.c
@@ -1,3 +1,5 @@
+#include <time.h>
+
 #ifndef REMOTE_C
 #define REMOTE_C
 
@@ -15,36 +17,40 @@
   #define LONG_HELD 2
 
   typedef struct{
+    char joystick;
+    char buttonGroup;
+    char button;
+
     int port;
     int pushed;  // temporary variable to hold that the button has been pushed
     int status;  // 1: yes push and released; 0 no
     int timePushed;
   } PRB_type;
 
-  static PRB_type PRB[NUM_PR_BUTTONS]; //Replace NUM_PR_BUTTONS
+  static PRB_type PRB[NUM_PR_BUTTONS];
 
   void updatePrbStatus(){
     int sv = 0;     // temp variable to hold sensor value
     for(int i = 0; i < NUM_PR_BUTTONS; i++){
-      sv = vexRT[PRB[i].port]; //TODO Replace vexRT
+      sv = joystickGetDigital(PRB[i].joystick, PRB[i].buttonGroup, PRB[i].button);
 
       // When button is pushed
       if (sv == 1){
-      	if(PRB[i].pushed == 0) PRB[i].timePushed = time1[T1]; //TODO Where is time1[T1] defined
+      	if(PRB[i].pushed == 0) PRB[i].timePushed = millis(); //TODO Where is time1[T1] defined
         PRB[i].pushed = 1;
       }
       // Alreay pushed and now released
       if (sv == 0 && PRB[i].pushed == 1){
         PRB[i].pushed = 0;
-        if(time1[T1] - PRB[i].timePushed >= 1000){
+        if(millis() - PRB[i].timePushed >= 1000){
           #if DEBUG == 1 || DEBUG_REMOTE == 1
-            writeDebugStreamLine("[REMOTE] Button %d has been long held", i);
+            printf("[REMOTE] Button %d has been long held", i);
           #endif
           PRB[i].status = 2;
         }
         else {
           #if DEBUG == 1 || DEBUG_REMOTE == 1
-            writeDebugStreamLine("[REMOTE] Button %d has been pushed and released", i);
+            printf("[REMOTE] Button %d has been pushed and released", i);
           #endif
           PRB[i].status = 1;
         }
