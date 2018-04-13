77de71fce83a7b6b74c59e9eb269d856bfbf3a59
Bill Qin <bzqin88@gmail.com>
2018-04-13 19:16:23 -0400

    Implemented double-press functionality (untested)

--- Bin/remote.c
+++ Bin/remote.c
@@ -13,7 +13,11 @@
 
   #define PUSHED_RELEASED 1
   #define LONG_HELD 2
-  #define DOUBLE_PRESS_TIME 300
+  #define DOUBLE_PRESS 3
+  #define TRANSITION_STATE -1
+
+#define DOUBLE_PRESS_TIME 200
+#define LONG_PRESS_TIME 1000
 
   typedef struct{
     char joystick;
@@ -21,9 +25,9 @@
     char button;
 
     int pushed;  // temporary variable to hold that the button has been pushed
-    int status;  // 1: yes push and released; 0 no
+    int status;  // 3: double; 2: long pushed; 1: yes push and released; 0: no; -1: within the time allot
     int timePushed;
-    int lastTimePushed;
+    int timeReleased;
   } PRB_type;
 
   static PRB_type PRB[NUM_PR_BUTTONS];
@@ -34,24 +38,36 @@
       sv = joystickGetDigital(PRB[i].joystick, PRB[i].buttonGroup, PRB[i].button);
 
       // When button is pushed
+      if (PRB[i].status == TRANSITION_STATE){
+        if(millis() - PRB[i].timeReleased() >= DOUBLE_PRESS_TIME){
+            PRB[i].status = 0;
+        }
+      }
       if (sv == 1){
       	if(PRB[i].pushed == 0) PRB[i].timePushed = millis(); //TODO Where is time1[T1] defined
         PRB[i].pushed = 1;
       }
       // Already pushed and now released
       if (sv == 0 && PRB[i].pushed == 1){
+        PRB[i].timeReleased = millis();
         PRB[i].pushed = 0;
-        if(millis() - PRB[i].timePushed >= 1000){
+        if(PRB[i].status() == -1){
+            #if DEBUG == 1 || DEBUG_REMOTE == 1
+                printf("[REMOTE] Button %d has been double pressed", i);
+            #endif
+            PRB[i].status = DOUBLE_PRESS;
+        }
+        else if(millis() - PRB[i].timePushed >= LONG_PRESS_TIME){
           #if DEBUG == 1 || DEBUG_REMOTE == 1
             printf("[REMOTE] Button %d has been long held", i);
           #endif
-          PRB[i].status = 2;
+          PRB[i].status = LONG_HELD;
         }
         else {
           #if DEBUG == 1 || DEBUG_REMOTE == 1
             printf("[REMOTE] Button %d has been pushed and released", i);
           #endif
-          PRB[i].status = 1;
+          PRB[i].status = PUSHED_RELEASED;
         }
       }
     }
@@ -68,7 +84,8 @@
   }
 
   void resetPrButton(int i){
-    PRB[i].status = 0;
+    if(PRB[i].status == PUSHED_RELASED) PRB[i].status = TRANSITION_STATE;
+    else PRB[i].status = 0;
   }
 
 #endif
