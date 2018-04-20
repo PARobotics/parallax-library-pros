ca1fc9ddfa2743307f9c76135444c3e39ff879da
Bill Qin <bzqin88@gmail.com>
2018-04-20 19:12:18 -0400

    Fixed bounds for sensorReset and fixed spacing

--- src/sensors.c
+++ src/sensors.c
@@ -7,10 +7,10 @@ void sensorUpdateVal(&Sensor) { // updates just val
 
 }
 void sensorReset() {
-  for(int n = 0; n < 26; n++){
+  for(int n = 1; n < 27; n++){
     digitalWrite(n, false);
   }
   for(int m = 0; m < IME_ADDR_MAX; m++){
-      imeReset(m);
+    imeReset(m);
   }
 }
